fd94adea45cf5cf5545519e6f27868a482ca46c5
ArlindFeta <arlindfeta433@gmail.com>
2026-02-26 10:10:43 +0100

    G

--- my-app/src/products/Mossi.tsx
+++ my-app/src/products/Mossi.tsx
@@ -18,7 +18,7 @@ const Mossi = () => {
     const fetchMossiLondonProducts = async () => {
       try {
         const response = await fetch(
-          "https://localhost:7132/api/products/by-name?keyword=Mossi London"
+          "https://localhost:7132/api/products/by-name?keyword=Mossi"
         );
 
         if (!response.ok) {
@@ -50,29 +50,33 @@ const Mossi = () => {
     <Layout>
     <section className="section1">
     <div>
-      <h1>The Mossi London Products</h1>
+      <h1 className="mossih1">The Mossi London Products</h1>
 
       {products.length === 0 && <p>No products found.</p>}
 
       <div style={{ display: "grid", gap: "16px" }}>
         {products.map((product) => (
           <div
-            key={product.id}
-            style={{
-              border: "1px solid #ccc",
-              padding: "12px",
-              borderRadius: "8px",
-            }}
+             key={product.id}
+              style={{
+                border: "1px solid #ccc",
+                padding: "12px",
+                borderRadius: "8px",
+                display:"",              
+                justifyContent: "space-between", 
+                alignItems: "flex-start",     
+                gap: "20px",                
+              }}
           >
             <h3>{product.name}</h3>
             <p>{product.description}</p>
-            <img src="/partners/pictures/vitamin.png" className="partner-logo" />
             <p>
               <strong>Components:</strong> {product.components}
             </p>
             <p>
               <strong>Price:</strong> {product.price.toFixed(2)} €
             </p>
+            <img src="/partners/pictures/vitamin.png" className="partner-logo" />
           </div>
         ))}
       </div>
