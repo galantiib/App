50dcaadca56109820a6a0bcee8f9bf151ac326c6
ArlindFeta <arlindfeta433@gmail.com>
2026-06-09 13:31:40 +0200

    apet

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
  import App from './App.js';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
