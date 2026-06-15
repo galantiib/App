96ed972c68b26f2e0a91d6666892a2eae8dd5521
ArlindFeta <arlindfeta433@gmail.com>
2026-06-15 12:12:56 +0200

    ggggggg

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
  import App from './App.js';
+ import About from './Main.tsx';
+ import Products from "./Products.tsx";
+import Contact from "./Contact.tsx";
+import Partners from "./Partners.tsx"
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter basename='/App'>
+    <BrowserRouter >
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 reportWebVitals();
