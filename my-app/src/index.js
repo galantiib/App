9f5088cd07b7b05f0c0cdc4527ed4a34afc644ef
ArlindFeta <arlindfeta433@gmail.com>
2026-06-15 13:12:31 +0200

    gg

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,16 +3,13 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
  import App from './App.js';
- import About from './Main.tsx';
- import Products from "./Products.tsx";
-import Contact from "./Contact.tsx";
-import Partners from "./Partners.tsx"
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter >
+    <BrowserRouter basename='/App' >
     <App />
   </BrowserRouter>
 );
